ebb1ac20a0b5cc1356e491016bb48f148077f7a5
Joao Gui <joaoguidev@gmail.com>
2021-03-28 12:42:55 -0700

    testing

---
+++ js/index.js
@@ -0,0 +1,65 @@
+
+
+import * as THREE from '../node_modules/three/build/three.module.js';
+import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import * as dat from 'dat.gui'
+
+
+const clock = new THREE.Clock();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+//renderer.setClearColor("#e5e5e5");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
+// const geometry = new THREE.SphereGeometry(1, 10, 10)
+// const material = new THREE.MeshLambertMaterial({ color: 0xffff00 })
+// const sphere = new THREE.Mesh(geometry, material)
+// scene.add(sphere)
+// const light = new THREE.PointLight( 0xff0000, 1, 500 );
+// light.position.set( 10, 0, 25 );
+
+// const controls = new THREE.OrbitControls(camera, renderer.domElement)
+// controls.update();
+
+const light = new THREE.AmbientLight(0x404040, 5); // soft white light
+scene.add(light);
+
+const loader = new GLTFLoader();
+
+loader.load(
+    "textures/Models/GLTF/truck_green.glb",
+    function (gltf) {
+        scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+        console.error(error);
+    }
+);
+
+const animate = function () {
+    const deltaTime = clock.getDelta();
+    console.log(deltaTime);
+    //sphere.rotation.x += 3 * deltaTime;
+    //sphere.rotation.y += 3 * deltaTime;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+};
+
+animate();
